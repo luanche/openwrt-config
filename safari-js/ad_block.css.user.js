1461109f3ce101c6f1cf245a0f8fb03fffb8bd3a
luanche <1054129686@qq.com>
2022-07-14 00:37:56 +0800

    css

---
+++ safari-js/ad_block.css.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name          Ad block css
+// @description	  Block ad by css
+// @author        luanche
+// @match         https://91porn.com/*
+// @run-at        document-start
+// ==/UserScript==
+
+(function() {var css = [
+	".ad_img {",
+	"  display: none!important;   ",
+	"}",
+].join("\n");
+if (typeof GM_addStyle != "undefined") {
+	GM_addStyle(css);
+} else if (typeof PRO_addStyle != "undefined") {
+	PRO_addStyle(css);
+} else if (typeof addStyle != "undefined") {
+	addStyle(css);
+} else {
+	var node = document.createElement("style");
+	node.type = "text/css";
+	node.appendChild(document.createTextNode(css));
+	var heads = document.getElementsByTagName("head");
+	if (heads.length > 0) {
+		heads[0].appendChild(node);
+	} else {
+		// no head yet, stick it whereever
+		document.documentElement.appendChild(node);
+	}
+}
+})();
